d744148169dd754116d6a011b245f13a2a4b1cb7
Yakuchi <lithmcfluffy@gmail.com>
2017-11-24 16:43:17 +0200

    Starting project

---
+++ src/main/java/pew/PoxNewsApplication.java
@@ -0,0 +1,13 @@
+
+package pew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoxNewsApplication {
+    
+    public static void main(String[] args){
+        SpringApplication.run(PoxNewsApplication.class, args);
+    }
+}
---
+++ src/main/java/pew/controller/DefaultController.java
@@ -0,0 +1,14 @@
+
+package pew.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+    
+    @RequestMapping("/")
+    public String handleDefault(){
+        return "Terve! /o/";
+    }
+}
