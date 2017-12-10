01a54b8c176c8543c536a399ea9c08dc839dba74
Yakuchi <lithmcfluffy@gmail.com>
2017-12-10 06:53:59 +0200

    Jekkutesti

---
+++ src/main/java/pew/controller/NewsController.java
@@ -0,0 +1,62 @@
+
+package pew.controller;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pew.domain.Author;
+import pew.domain.Category;
+import pew.domain.NewsObject;
+import pew.repository.AuthorRepository;
+import pew.repository.CategoryRepository;
+import pew.repository.NewRepository;
+
+@Controller
+public class NewsController {
+    
+    @Autowired
+    private AuthorRepository autoRepo;
+    
+    @Autowired
+    private CategoryRepository catRepo;
+    
+    @Autowired
+    private NewRepository newRepo;
+    
+    @GetMapping("/news")
+    public String home(Model model){
+        model.addAttribute("news", newRepo.findAll());
+        return "news";
+    }
+    
+    @Transactional
+    @GetMapping("/news/{id}")
+    public String manage(Model model, @PathVariable Long id){
+        NewsObject n = newRepo.getOne(id);
+        model.addAttribute("newsObject", n);
+        n.view();
+        return "new";
+    }
+    
+    @Transactional
+    @GetMapping("/news/{id}/image")
+    public ResponseEntity<byte[]> getImage(@PathVariable Long id){
+        NewsObject n = newRepo.getOne(id);
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(n.getContentType()));
+        headers.setContentLength(n.getSize());
+        
+        return new ResponseEntity<>(n.getImage(), headers, HttpStatus.CREATED);
+    }
+}
--- src/main/java/pew/domain/NewsObject.java
+++ src/main/java/pew/domain/NewsObject.java
@@ -1,13 +1,23 @@
 
 package pew.domain;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.persistence.Basic;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
+import javax.persistence.MapKeyColumn;
 import javax.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -38,8 +48,49 @@ public class NewsObject extends AbstractPersistable<Long>{
     @ManyToMany
     private List<Category> categories;
     
+    @ElementCollection
+    @CollectionTable(name = "views")
+    @MapKeyColumn(name = "date")
+    @Column(name = "amount")
+    private Map<String, Long> views;
+    
     public NewsObject() {
         this.releaseDate = LocalDateTime.now();
+        views = new HashMap<String, Long>();
+    }
+    
+    public void view(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        String s = dateFormat.format(date);
+        if(views.containsKey(s)) {
+            views.put(s, views.get(s)+1);
+        }else{
+            views.put(s, 1L);
+        }
+    }
+    
+    public Long getViews(){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        Date date = new Date();
+        return views.get(dateFormat.format(date));
+    }
+    
+    public Long getViewsFromWeek(){
+        if(this.views.isEmpty()) return 0L;
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        LocalDate today = LocalDate.now();
+        Long result = getViews();
+        if(this.views.size() < 7){
+            for(int i=1; i<this.views.size(); i++){
+                result += this.views.get(dateFormat.format(today.minusDays(i)));
+            }
+        }else{
+            for(int i=1; i<7; i++){
+                result += this.views.get(dateFormat.format(today.minusDays(i)));
+            }
+        }
+        return result;
     }
     
     public void addAuthor(Author param){
